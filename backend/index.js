d13eda6ada37276fbf3e72483f20221823c144e9
youneshimi <ounesshimi@gmail.com>
2023-03-20 15:06:14 +0000

    first commit

---
+++ backend/index.js
@@ -0,0 +1,54 @@
+
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+var cors = require('cors')
+app.use(cors())
+
+
+
+const multer = require("multer")
+const path = require("path")
+
+const authRoute = require("./routes/auth")
+const authUser = require("./routes/user")
+const authPost = require("./routes/posts")
+const authCat = require("./routes/categories")
+
+dotenv.config()
+
+app.use(express.json())
+
+app.use("/images", express.static(path.join(__dirname, "/images")))
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err))
+
+const storage = multer.diskStorage({
+  destination: (req, file, callb) => {
+    callb(null, "images")
+  },
+  filename: (req, file, callb) => {
+    callb(null, req.body.name)
+  },
+})
+const upload = multer({ storage: storage })
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded")
+})
+
+app.use("/auth", authRoute)
+app.use("/users", authUser)
+app.use("/posts", authPost)
+app.use("/category", authCat)
+
+app.listen("5000", () => {
+  console.log("backend running...")
+})
---
+++ backend/model/category.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+});
+module.exports = mongoose.model("Category", CategorySchema);
+
+
+
